fd90d7cd5b610b5f79952b27cf000335ab5f7c0b
Usuario Local <UsuarioLocal@EQUIP003027.xunta.local>
2022-02-07 13:41:03 +0100

    JS carrusel, y clases CSS
    -creamos en el JS la aplicación del carrusel (cambio de imagen automatico y con botones)

--- design/js/app.js
+++ design/js/app.js
@@ -17,3 +17,69 @@ document.addEventListener(
     }
 )
 
+// Carrusel
+
+const imagenesCarrusel = document.querySelectorAll(".carrusel-image");
+const puntosCarrusel = document.querySelectorAll(".carrusel-position");
+const botonatras = document.querySelector(".carrusel-prev");
+const botonadelante = document.querySelector(".carrusel-next");
+
+var contador = 0;
+
+window.onload = function(){
+    puntosCarrusel[0].classList.add("posicionado");
+    imagenesCarrusel[contador].classList.add("carruselmostrado");
+}
+
+const limpiar = () => {
+    puntosCarrusel.forEach(
+        elemento =>{
+            elemento.classList.remove("posicionado")
+        }
+    );
+    imagenesCarrusel.forEach(
+        elemento =>{
+            elemento.classList.remove("carruselmostrado")
+        }
+    );  
+}
+
+botonatras.addEventListener(
+    "click",
+    ()=>
+    {
+        limpiar();
+        contador--
+        if(contador <0){
+            contador = 3
+            console.log(contador)
+        }
+        puntosCarrusel[contador].classList.add("posicionado")
+        imagenesCarrusel[contador].classList.add("carruselmostrado");
+    }
+);
+
+botonadelante.addEventListener(
+    "click",
+    ()=>{
+        limpiar();
+        contador++
+        if(contador >3){
+            contador = 0
+            console.log(contador)
+        }
+        puntosCarrusel[contador].classList.add("posicionado")
+        imagenesCarrusel[contador].classList.add("carruselmostrado")
+    }
+);
+
+setInterval(function(){
+    contador++;
+    if(contador >3){
+        contador = 0;
+    }
+    limpiar();
+    puntosCarrusel[contador].classList.add("posicionado")
+    imagenesCarrusel[contador].classList.add("carruselmostrado")
+    console.log(contador)
+},12000)
