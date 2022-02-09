ddad0a6d3935e5a3a4036a053f07d5deabcc2393
xurxogonzalez <xurxo@webferrol.com>
2022-02-09 14:19:40 +0100

    Añadido vista carrusel.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
+    <!-- Vista de la cabecera página -->
     <header-section></header-section>
+
+    <!-- Carrusel de fotos -->
+    <carrusel-vue></carrusel-vue>
+  
+
+    <!-- Vista del pie de página -->
     <footer-section></footer-section>
   </div>
 </template>
@@ -9,12 +16,14 @@
 // @ is an alias to /src
 import HeaderSection from './page_structure/HeaderSection.vue';
 import FooterSection from './page_structure/FooterSection.vue'
+import CarruselVue from './Carrusel.vue';
 
 export default {
   name: 'Home',
   components: {
     HeaderSection,
-    FooterSection
+    FooterSection,
+    CarruselVue
   }
 }
 </script>
