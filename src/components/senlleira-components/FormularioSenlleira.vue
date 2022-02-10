3e93bd6475f257f9831f8930b83eecb1d611ac8d
webferrol <xurxo@webferrol.com>
2022-02-10 13:23:52 +0100

    Creado el componente geolocation y modificación del formulario de inserción de senlleiras

--- src/components/senlleira-components/FormularioSenlleira.vue
+++ src/components/senlleira-components/FormularioSenlleira.vue
@@ -1,40 +1,82 @@
 <template>
-    <form class="senlleiras" @submit.prevent>
+    <form class="senlleiras" @submit.prevent="submit">
         <ul class="fields">
             <li class="field">
                 <label for="especie">Especie</label>
-                <select v-model="especie" name="especie" id="especie">
-                    <option value="Desconocida"></option>
-                    <option 
-                    v-for="item in species" 
-                    :key="item.id" 
-                    :value="item.id">
-                    {{item.genus}} {{item.specie}}
-                    </option>
+                <select @change="obtenerNombreComun" v-model="especie" name="especie" id="especie">
+                    <option value="Desconocida">Lo ignoro</option>
+                    <option
+                        v-for="item in species"
+                        :key="item.id"
+                        :value="item.id"
+                    >{{ item.genus }} {{ item.specie }}</option>
                 </select>
             </li>
-        </ul>        
+            <li class="field">
+                <label for="nombrecomun">Nombre común</label>
+                <input
+                    placeholder="Su texto aquí"
+                    type="text"
+                    v-model.trim="nombreComun"
+                    name="nombrecomun"
+                    id="nombrecomun"
+                />
+            </li>
+            <li class="field">                
+                <the-geolocation :location="location"></the-geolocation>
+            </li>
+            <li class="field">
+                <label for="comentarios">Comentarios</label>
+                <textarea
+                    placeholder="Su texto aquí"
+                    v-model.trim="comentarios"
+                    name="comentarios"
+                    id="comentarios"
+                ></textarea>
+            </li>
+        </ul>
+        <button>Submit</button>
     </form>
 </template>
 
 <script setup>
-import { onMounted,computed,ref } from 'vue';
+import TheGeolocation from '../TheGeolocation.vue';
+import { onMounted, computed, ref, reactive } from 'vue';
 import { useStore } from 'vuex';
+
 //Accedemos al Store de Vuex
 const store = useStore();
+//Campos del formulario
+let especie = ref('');
+let nombreComun = ref('');
+let comentarios = ref('');
 
-const especie = ref('');
+const location = reactive({
+    latitude: null,
+    longitude: null
+});
 
 //Ciclo de vida
-onMounted(()=>{
+onMounted(() => {
     store.dispatch('setSpecies');
 });
 
 //Para obtener los datos del store hay que utilizar los métodos compudados
-const species = computed(()=>{
+const species = computed(() => {
     return store.getters.getSpecieSort;
 });
 
-//obtenomos los datos
+const obtenerNombreComun = e => {
+    //console.log(e.target.value)
+    //console.log(species.value)
+    nombreComun.value = '';
+    const specie = species.value.find(element => element.id === e.target.value);
+    if (specie) nombreComun.value = specie.names.join();
+}
 
+const submit = () => {
+    console.log(
+        especie.value, nombreComun.value,location
+    );
+}
 </script>
