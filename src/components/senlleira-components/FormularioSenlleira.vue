54e4a5eb269889afce646d4f20dfc6df6a4f6dda
webferrol <xurxo@webferrol.com>
2022-02-10 12:25:40 +0100

    Añadidas las especies en el store y en el formulario para añadir senlleiras

--- src/components/senlleira-components/FormularioSenlleira.vue
+++ src/components/senlleira-components/FormularioSenlleira.vue
@@ -1,58 +1,40 @@
 <template>
-    <section class="titulo-administracion">
-        <h2>Cargar Información</h2>
-        <p>Inserta la información que estará disponible para el usuario</p>
-
-        <form @submit.prevent class="form-administrativo" action="#" method="post">
-            <div class="administrativo-input">
-                <input type="text" id="genero" name="arbol_genero" placeholder="Género" />
-                <input type="text" id="especie" name="arbol-especie" placeholder="Especie" />
-                <input type="text" id="nombre" name="arbol-nombre" placeholder="Nombre" />
-            </div>
-            <textarea id="description" rows="4" cols="100" placeholder="Descripción"></textarea>
-            <input class="administrativo-submit" type="submit" value="Cargar" />
-        </form>
-    </section>
+    <form class="senlleiras" @submit.prevent>
+        <ul class="fields">
+            <li class="field">
+                <label for="especie">Especie</label>
+                <select v-model="especie" name="especie" id="especie">
+                    <option value="Desconocida"></option>
+                    <option 
+                    v-for="item in species" 
+                    :key="item.id" 
+                    :value="item.id">
+                    {{item.genus}} {{item.specie}}
+                    </option>
+                </select>
+            </li>
+        </ul>        
+    </form>
 </template>
 
-<style scoped>
-.titulo-administracion h2 {
-    text-align: center;
-    text-transform: uppercase;
-    font-size: 30px;
-    padding: 10px;
-}
-.titulo-administracion p {
-    font-size: 25px;
-    text-align: center;
-}
-.form-administrativo {
-    display: grid;
-    gap: 5px;
-    font-size: 25px;
-    min-width: 320px;
-    max-width: 50%;
-    margin: 20px auto;
-}
-.form-administrativo .administrativo-input {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
-    gap: 5px;
-}
-.form-administrativo .administrativo-submit {
-    width: 100px;
-    height: 50px;
-}
-.form-administrativo input {
-    height: 30px;
-    font-size: 25px;
-    font-family: montserrat;
-}
-.form-administrativo textarea {
-    resize: none;
-    width: 100%;
-    font-size: 25px;
-    font-family: montserrat;
-    min-width: 320px;
-}
-</style>
+<script setup>
+import { onMounted,computed,ref } from 'vue';
+import { useStore } from 'vuex';
+//Accedemos al Store de Vuex
+const store = useStore();
+
+const especie = ref('');
+
+//Ciclo de vida
+onMounted(()=>{
+    store.dispatch('setSpecies');
+});
+
+//Para obtener los datos del store hay que utilizar los métodos compudados
+const species = computed(()=>{
+    return store.getters.getSpecieSort;
+});
+
+//obtenomos los datos
+
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,52 @@
 import { createStore } from 'vuex'
 
+//URL del listado de especies
+const SPECIES = 'https://arbores-senlleiras-default-rtdb.europe-west1.firebasedatabase.app/species.json';
+
 export default createStore({
   state: {
+    species : []
   },
   mutations: {
+    setSpecies(state,payload){
+      state.species = payload;
+    }
   },
   actions: {
+    async setSpecies(context){
+      try {
+        const response = await fetch(SPECIES,{
+          method : 'GET',
+          headers: {
+            'content-type' : 'application/json'
+          }
+        });
+
+        //Los datos obtenidos es un objeto con objetos en su interior
+        //{{},{},{}}
+        const data = await response.json();
+        //console.log(data)
+
+        //Creamos un array temporal vacío
+        const tmp = [];
+
+        //for...in. La instrucción for-in itera sobre todas las "propiedades enumerables" de un objeto que está codificado por cadenas
+        for(let item in data){
+          tmp.push(data[item]);
+          //console.log(data[item])
+        }
+        //Pasamos al state
+        context.commit('setSpecies',tmp);
+
+      } catch (error) {
+        console.log(`Error getSpeciesList en store/index.js: ${error}`);
+      }
+    }
+  },
+  getters: {
+    getSpecieSort(state){
+      return state.species.sort((x,y)=>x.specie.localeCompare(y.specie))
+    }
   },
   modules: {
   }
