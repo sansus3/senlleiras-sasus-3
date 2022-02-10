dcb08745e86025a650e07d1e96494ab9ecc52e65
webferrol <xurxo@webferrol.com>
2022-02-10 20:08:28 +0100

    Actualizado el menú superior con css y estructura html

--- src/views/page_structure/HeaderSection.vue
+++ src/views/page_structure/HeaderSection.vue
@@ -1,19 +1,88 @@
 <template>
-    <nav class="main-header header-nav-app">
-        <a href="#" class="header-logo">lOGO</a>
-        <ul class="header-nav-buttons">
-            <li class="header-search">
-                <i class="fas fa-search"></i>
-                <input type="search" placeholder="Buscar" />
-            </li>
-            <li class="header-upload">
-                <router-link :to="{ name: 'About' }">
-                    <i class="fas fa-tree"></i>
-                </router-link>
-            </li>
-            <li class="header-profile">
-                <i class="fas fa-user-circle"></i>
-            </li>
-        </ul>
-    </nav>
+    <header class="main-header">
+        <nav class="header-nav-app">
+            <a href="#" class="header-logo">lOGO</a>
+            <ul class="header-nav-buttons">
+                <li class="header-search">
+                    <i class="fas fa-search"></i>
+                    <input type="search" placeholder="Buscar" />
+                </li>
+                <li class="header-upload">
+                    <a href="#">
+                        <i class="fas fa-tree"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
+
+<style scoped>
+/* CABECERA TOP */
+
+.header-nav-app {
+    position: fixed;
+    top: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 40px;
+    background-color: var(--colorprincipal);
+    transition: all ease-in-out 0.3s;
+    z-index: 1;
+}
+.header-nav-buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+}
+.header-nav-app .header-logo {
+    font-size: 30px;
+    width: 60px;
+    color: var(--colortitulo);
+    text-transform: uppercase;
+    font-weight: 900;
+    margin: 0;
+    padding: 0 10px;
+}
+.header-nav-buttons .header-search {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    color: var(--colortitulo);
+    font-size: 15px;
+    padding: 5px;
+}
+.header-nav-buttons .header-search i {
+    padding: 3px;
+    color: white;
+}
+.header-nav-buttons .header-search input {
+    width: 60%;
+}
+.header-nav-buttons .header-search input:focus {
+    outline: none;
+}
+.header-nav-buttons .header-upload a {
+    font-size: 20px;
+    color: var(--colortitulo);
+    padding: 10px;
+}
+
+/* Cabecera se encoge/ensancha */
+
+.reduced {
+    height: 0px;
+    transition: all ease-in-out 0.3s;
+}
+.reduced .header-logo {
+    display: none;
+}
+.reduced .header-nav-buttons {
+    display: none;
+}
+</style>
