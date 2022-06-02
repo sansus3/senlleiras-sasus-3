b21508eaaa2b10af79028f1b7ff77623b74927c8
Martin <gagochorenmartin@gmail.com>
2022-06-02 22:53:46 +0200

    Diseño Stories arbores
    - Imagenes de mayor tamaño
    - Ajuste en el carrusel

--- src/components/senlleira-components/ArbolStories.vue
+++ src/components/senlleira-components/ArbolStories.vue
@@ -1,37 +1,41 @@
 <template>
-  <section class="arbol-stories">
-      <h2>Árbol del día</h2>
-      <div class="stories-navigator">
-          <div class="component-arbol">
-              <img src="../../assets/gallery/senlleiras_17.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-              <img src="../../assets/gallery/senlleiras_13.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-              <img src="../../assets/gallery/senlleiras_11.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-            <img src="../../assets/gallery/senlleiras_16.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-            <img src="../../assets/gallery/senlleiras_7.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-            <img src="../../assets/gallery/senlleiras_8.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-            <img src="../../assets/gallery/senlleiras_6.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-            <img src="../../assets/gallery/senlleiras_12.jpg" alt="sdsd">
-          </div>
-          <div class="component-arbol">
-            <img src="../../assets/gallery/senlleiras_1.jpg" alt="sdsd">
-          </div>
-      </div>
+  
+    <div class="stories-navigator">
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_17.jpg" alt="sdsd">
+        <p class="arbol-name"> Loureiro</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_1.jpg" alt="sdsd">
+        <p class="arbol-name"> Carballo</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_2.jpg" alt="sdsd">
+        <p class="arbol-name"> Encina</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_3.jpg" alt="sdsd">
+        <p class="arbol-name"> Alcornoque</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_4.jpg" alt="sdsd">
+        <p class="arbol-name"> Ciprés</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_5.jpg" alt="sdsd">
+        <p class="arbol-name"> Sauce</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_6.jpg" alt="sdsd">
+        <p class="arbol-name"> haya</p>
+      </router-link>
+      <router-link to="/" class="component-arbol">
+        <img src="../../assets/gallery/senlleiras_7.jpg" alt="sdsd">
+        <p class="arbol-name"> Eucalipto</p>
+      </router-link>
+      
+    </div>
 
-  </section>
 </template>
 
 <script>
@@ -40,22 +44,52 @@
 
 <style>
 ::-webkit-scrollbar {
-    display: none;
-    }
-
-.stories-navigator{
-    display: grid;
-    grid-template-columns: repeat( 10, minmax(50px, 60px));
-    gap: .5rem;
-    overflow: scroll;
-    scroll-behavior: smooth;
+  display: none;
 }
-    .stories-navigator .component-arbol img{
-        width: 50px;
-        height: 50px;
-        border: 3px solid green;
-        border-radius: 50%;
-        margin: .5rem;
-    }
 
+.stories-navigator {
+  display: flex;
+  gap: .2rem;
+  margin: .5rem 0;
+  overflow: scroll;
+  scroll-behavior: smooth;
+  align-items: center;
+}
+
+.component-arbol {
+  display: grid;
+  grid-template-rows: auto auto;
+  align-items: center;
+  align-content: center;
+  width: 80px;
+  gap: .1rem;
+}
+.component-arbol:nth-child(1){
+  margin-left: 1.5rem;
+}
+
+.stories-navigator .component-arbol img {
+  margin: auto;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  padding: .2rem;
+  border: 4px solid green;
+  object-fit: fill;
+  position: relative;
+}
+.stories-navigator .component-arbol:visited img{
+  border: 4px solid gray
+}
+
+.stories-navigator .arbol-name {
+  width: 80px;
+  font-size: .9rem;
+  line-height: .9rem;
+  text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: var(--colorprincipal);
+}
 </style>
